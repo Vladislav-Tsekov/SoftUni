899289fd34886c00b4fe94a1bce4b9a998694c49
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-10-08 21:23:52 +0300

    Add files via upload

---
+++ FundamentalKnowledge/Arrays/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+int n = int.Parse(Console.ReadLine());
+
+int[] firstArray = new int[n];
+int[] secondArray = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int[] currentN = Console.ReadLine()
+        .Split(" ")
+        .Select(int.Parse)
+        .ToArray();
+
+    if (i % 2 == 0)
+    {
+        firstArray[i] = currentN[0];
+        secondArray[i] = currentN[1];
+    }
+    else
+    {
+        firstArray[i] = currentN[1];
+        secondArray[i] = currentN[0];
+    }
+}
+
+Console.WriteLine(string.Join(" ", firstArray));
+Console.WriteLine(string.Join(" ", secondArray));
