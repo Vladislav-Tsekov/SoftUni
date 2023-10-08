54fff3b636effe974d1467777a73a272e5555ecc
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-10-08 21:28:00 +0300

    Add files via upload

---
+++ FundamentalKnowledge/Arrays/09. Kamino Factory/Program.cs
@@ -0,0 +1,47 @@
+﻿
+int dnaLength = int.Parse(Console.ReadLine());
+
+int[] dnaArray = { dnaLength };
+int dnaSequence = 1;
+int maxSequence = 0;
+int sample = 0;
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "Clone them!")
+    {
+        break;
+    }
+
+    sample++;
+
+    int[] dna = input
+    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+    foreach (var item in dna)
+    {
+        if (item == 1)
+        {
+            dnaSequence++;
+        }
+        if (dnaSequence > maxSequence)
+        {
+            maxSequence = dnaSequence;
+        }
+        else if (item == 0)
+        {
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"{dnaSequence}, {maxSequence}, {sample}, {bestSample}");
+
+
+
+
+
