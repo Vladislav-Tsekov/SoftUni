0dc2eaf3d7e701909e7cf99880efc010de12eb90
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-10-08 21:25:21 +0300

    Add files via upload

---
+++ FundamentalKnowledge/Arrays/04. Array Rotation/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int j = 0; j < rotations; j++)
+{
+    int firstElement = numbers[0];
+
+    for (int i = 0; i < numbers.Length - 1; i++)
+    {
+        numbers[i] = numbers[i + 1];
+    }
+
+    numbers[numbers.Length - 1] = firstElement;
+}
+
+Console.WriteLine(string.Join(" ", numbers));
---
+++ FundamentalKnowledge/Arrays/05. Top Integers/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Input
+//                          Output
+//1 4 3 2                   4 3 2
+//14 24 3 19 15 17          24 19 17
+//27 19 42 2 13 45 48       48
+
+
+int[] numbers = Console.ReadLine()  //1 4 3 2
+.Split(" ")
+.Select(int.Parse)
+.ToArray();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    bool isTop = true;
+	for (int j = i + 1; j < numbers.Length; j++)
+	{
+		if (numbers[i] <= numbers[j])
+		{
+			isTop = false;
+			break;
+		}
+	}
+	if (isTop)
+	{
+		Console.Write($"{numbers[i]} ");
+	}
+
+}
