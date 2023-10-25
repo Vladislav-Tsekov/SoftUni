a683ea48be2efc7fc06385bbcdda90db32bd704d
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-10-25 14:47:43 +0300

    Add files via upload

---
+++ FundamentalKnowledge/Lists/01. Messaging/Program.cs
@@ -0,0 +1,36 @@
+﻿List<int> numbers = Console.ReadLine() // 11 2 32 43 331 522 441 2241 711 1821
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+List<int> numbersSum = new List<int>();
+
+string text =  Console.ReadLine(); //69da343n44ge96rous311!
+string newText = string.Empty; // 2 2 5 7 7 9..
+
+foreach (var num in numbers)
+{
+    char[] numToChar = num.ToString().ToCharArray(); // 1+1, 2, 3+2, 4+3..
+    int totalSum = 0;
+
+    foreach (var ch in numToChar)
+        totalSum += int.Parse(ch.ToString());
+
+    numbersSum.Add(totalSum);
+}
+
+foreach (var num in numbersSum)
+{
+    int number = num;
+
+    while (number > text.Length)
+    {
+        number -= text.Length;
+    }
+    
+    string currentChar = text.Substring(number, 1);
+    newText += currentChar;
+    text = text.Remove(number, 1);
+}
+
+Console.WriteLine(newText);
---
+++ FundamentalKnowledge/Lists/02. Car Race/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int middle = numbers.Count / 2;
+
+List<int> leftCar = new List<int>();
+List<int> rightCar = new List<int>();
+
+for (int i = 0; i < middle; i++)
+{
+    leftCar.Add(numbers[i]);
+}
+for (int i = middle + 1; i < numbers.Count; i++)
+{
+    rightCar.Add(numbers[i]);
+}
+
+double totalTimeRC = 0;
+double totalTimeLC = 0;
+
+{
+foreach (var num in leftCar)
+    if (num == 0)
+        totalTimeLC = totalTimeLC * 0.8;
+    else
+        totalTimeLC += num;
+}
+foreach (var num in rightCar)
+{ 
+    if (num == 0)
+        totalTimeRC = totalTimeRC * 0.8;
+    else
+        totalTimeRC += num;
+}
+
+if (totalTimeLC > totalTimeRC)
+{
+    if (!rightCar.Contains(0))
+    {
+        Console.WriteLine($"The winner is right with total time: {totalTimeRC}");
+        return;
+    }
+    Console.WriteLine($"The winner is right with total time: {totalTimeRC}");
+}
+else if (totalTimeLC < totalTimeRC)
+{
+    if (!leftCar.Contains(0))
+    {
+        Console.WriteLine($"The winner is left with total time: {totalTimeLC}");
+        return;
+    }
+    Console.WriteLine($"The winner is left with total time: {totalTimeLC}");
+}
