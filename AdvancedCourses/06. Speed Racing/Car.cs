21185f3e68447b9f48b8b0fabaa5c546f5efe884
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-11-09 23:21:11 +0200

    Add files via upload

---
+++ AdvancedCourses/06. Speed Racing/Car.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace DefiningClasses;
+
+public class Car
+{
+    private string model; //VERIFICATION?
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+    public double FuelAmount { get; set; }
+    public double FuelConsumptionPerKilometer { get; set; }
+    public double TravelledDistance { get; set; }
+
+    public Car() { }
+    public Car(string model, double fuelAmount, double fuelConsumption)
+    {
+        Model = model;
+        FuelAmount = fuelAmount;
+        FuelConsumptionPerKilometer = fuelConsumption;
+    }
+
+    public void IsFuelSufficientOrNot(string model, double kmAmount)
+    {
+        double consumption = FuelConsumptionPerKilometer * kmAmount;
+
+        if (FuelAmount >= consumption)
+        {
+            FuelAmount -= consumption;
+            TravelledDistance += kmAmount;
+        }
+        else
+        {
+            Console.WriteLine($"Insufficient fuel for the drive");
+        }
+    }
+
+
+}
