a662734985e471a914dd1bf7d1b0bb0c01e5e4b8
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-11-19 22:56:51 +0200

    Add files via upload

---
+++ C#-Advanced/Matrices/07. Pascal Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+long size = long.Parse(Console.ReadLine());
+long[][] jArray = new long[size][];
+
+for (long row = 0; row < size; row++)
+{
+    jArray[row] = new long[row + 1];
+
+    for (long col = 0; col < row + 1; col++)
+    {
+        if (col > 0 && col < jArray[row].Length - 1)
+        {
+            jArray[row][col] = jArray[row - 1][col - 1] + jArray[row - 1][col];
+        }
+        else
+        {
+            jArray[row][col] = 1;
+        }
+    }
+}
+
+foreach (long[] element in jArray)
+{
+    Console.WriteLine(string.Join(' ', element));
+}
