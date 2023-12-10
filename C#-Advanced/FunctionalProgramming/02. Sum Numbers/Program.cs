e6eba6e6a7fe66d37e58e362c1994d1a2fae9301
Vladislav Tsekov <115028047+Vladislav-Tsekov@users.noreply.github.com>
2023-12-10 22:51:46 +0200

    Add files via upload

---
+++ C#-Advanced/FunctionalProgramming/02. Sum Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+int[] numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine($"{numbers.Length}");
+Console.WriteLine($"{numbers.Sum()}");
---
+++ C#-Advanced/FunctionalProgramming/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+string[] text = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+Func<string, bool> isUpper = c => Char.IsUpper(c[0]);
+
+string[] upperCase = text.Where(isUpper).ToArray();
+
+foreach (var word in upperCase)
+{
+    Console.WriteLine(word);
+}
